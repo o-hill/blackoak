acddca6438166a88598730d520f1d5b8b6c67bc6
Oliver <oliverhi@umich.edu>
2018-07-08 18:00:49 -0700

    Initial commit with the Deep RL labs done.

---
+++ scripting/gym_test.py
@@ -0,0 +1,16 @@
+'''
+
+    Testing OpenAi Gym functionailty.
+    It works!
+
+'''
+
+import gym
+
+
+env = gym.make('CartPole-v0')
+env.reset()
+
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample())
